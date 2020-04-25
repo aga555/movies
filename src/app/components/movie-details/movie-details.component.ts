3ecf944ed0305092c708b66e3f34bfb0606978bb
akrawczyk <agammkrawczyk@gmail.com>
2020-04-25 19:07:36 +0200

    use activatedRoute with params to display single movie.

--- src/app/components/movie-details/movie-details.component.ts
+++ src/app/components/movie-details/movie-details.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MoviesService} from '../../services/movies.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-movie-details',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-details.component.css']
 })
 export class MovieDetailsComponent implements OnInit {
+  movie: any;
 
-  constructor() { }
+  constructor(private  service: MoviesService,
+              private activatedRoute: ActivatedRoute) {
+  }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.activatedRoute.params
+      .subscribe(params => {
+        let id = params['imdbID'];
+        this.service.getMoviesDetails(id)
+          .subscribe(resp => this.movie = resp);
+      });
   }
 
 }
